ed2cbf0cfa7739111ee1ffacc68d7dbea9fce9dd
Sam <samuelkoh17@gmail.com>
2024-06-11 19:22:39 +0800

    feat: created react page for email and password login placeholders

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -3,7 +3,21 @@ import React from 'react'
 function Login() {
   return (
     <div>
-      
+      <div>
+        <form action=''>
+          <div classname='mb-3'>
+            <label htmlFor='email'>Email</label>
+            <input type='email' placeholder='Enter Email' />
+          </div>
+          <div classname='mb-3'>
+            <label htmlFor='password'>Password</label>
+            <input type='password' placeholder='Enter Password' />
+          </div>
+          <button className='btn btn-succcess'>Login</button>
+          <p> Don't have an account? <a href='/register'>Register</a></p>
+          <button className='btn btn-default border'>Create Account</button>
+        </form>
+      </div>
     </div>
   )
 }
