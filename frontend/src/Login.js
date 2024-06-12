db891688a58dc7021b8c8a312f871d8850b8f75a
Sam <samuelkoh17@gmail.com>
2024-06-12 21:36:00 +0800

    feat: implemented state initialisation, handling input, and handling submit at frontend

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,22 +1,48 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
-
+import './LoginValidation';
 
 
 function Login() {
+  const [values, setValues] = useState({
+    email: '',
+    password: ''
+  })
+
+  const [errors, setErrors] = useState({
+    email: '',
+    password: ''
+  })
+
+  const handleInput =(e) => {
+    setValues(prev => ({...prev, [e.target.name]: e.target.value}))
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setErrors(validation(values));
+  }
+
+
   return (
     <div className='d-flex justify-content-center align-items-center bg-primary vh-100'>
       <div className='bg-white p-3 rounded w-25'>
-        <form action=''>
+        <h2>Log In</h2>
+        <form action='' onSubmit={handleSubmit}>
           <div className='mb-3'>
             <label htmlFor='email'><strong>Email</strong></label>
-            <input type='email' placeholder='Enter Email' className='form-control rounded-0'/>
+            <input type='email' placeholder='Enter Email' name='email'
+            onChange={handleInput} className='form-control rounded-0'/>
           </div>
+
           <div className='mb-3'>
             <label htmlFor='password'><strong>Password</strong></label>
-            <input type='password' placeholder='Enter Password' className='form-control rounded-0'/>
+            <input type='password' placeholder='Enter Password' name='password'
+            onChange={handleInput} className='form-control rounded-0'/>
+
           </div>
-          <button className='btn btn-success w-100 rounded-0'><strong> Log In</strong></button>
+
+          <button type='submit' className='btn btn-success w-100 rounded-0'>Log In</button>
           <p> You agree to our terms and policies.</p>
           <Link to="/signup" className='btn btn-default border w-100 bg-light rounded-0 text-decoration-none'> Create Account</Link>
         </form>
